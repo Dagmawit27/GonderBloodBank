dc48b547c07894d2b3ad6221afa65218453e5016
Dagmawit Mesfin <Dagmawit27@users.noreply.github.com>
2026-04-28 18:15:32 +0300

    check in console

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
+if (!BASE_URL) {
+  console.error("VITE_API_URL is not defined");
+}
 export const checkUsername = async (username) => {
   const response = await fetch(
     `${BASE_URL}/donor/check-username?username=${encodeURIComponent(username)}`
